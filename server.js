a115bc9b3a6a203efd98d834d781fdfc44ed72fc
gingergaz <gaz@cliffhanger.com>
2021-09-13 17:39:42 +0100

    Add all get route handlers

--- server.js
+++ server.js
@@ -11,3 +11,18 @@ app.use(express.static("public"));
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+app.get("/api/quotes/random", (req, res, next) => {
+  const randomQuote = getRandomElement(quotes);
+  res.send({ quote: randomQuote });
+});
+
+app.get("/api/quotes", (req, res, next) => {
+  let quotesSelection = quotes;
+  if ("person" in req.query) {
+    quotesSelection = quotesSelection.filter((quoteObject) => {
+      return quoteObject.person === req.query.person;
+    });
+  }
+  res.send({ quotes: quotesSelection });
+});
